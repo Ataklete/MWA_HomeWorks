b6372327a2a010027f27ad59c5585a08b54b831c
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-10-04 22:00:41 -0500

    MWA Home work

---
+++ MWA-demo-angular01-master/ngapp/src/app/comp6/comp6.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, AfterContentChecked, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-comp6',
+  template: `
+    <h1>Component 6:</h1>
+    <p>Component LifeCycle: Check Console...</p>
+  `,
+  styles: []
+})
+export class Comp6Component implements OnInit, AfterContentChecked,
+  OnChanges {
+    constructor() { }
+
+    ngOnChanges() {
+        this.log('ngOnChanges');
+    }
+
+    ngOnInit() {
+        this.log('ngOnInit');
+    }
+
+    ngDoCheck() {
+        this.log('ngDoCheck');
+    }
+
+    ngAfterContentInit() {
+        this.log('ngAfterContentInit');
+    }
+
+    ngAfterContentChecked() {
+        this.log('ngAfterContentChecked');
+    }
+
+    ngAfterViewInit() {
+        this.log('ngAfterViewInit');
+    }
+
+    ngAfterViewChecked() {
+        this.log('ngAfterViewChecked');
+    }
+
+    ngOnDestroy() {
+        this.log('ngOnDestroy');
+    }
+
+   private log(msg: string) {
+        console.log(msg);
+    }
+
+
+}
